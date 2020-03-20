d3b813281b7da7d161e49bdba6f75f644c6493e2
Haitao Huang <hht1996@hotmail.com>
2020-03-20 03:37:24 -0700

    Finishing bonus user stories

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -14,31 +14,29 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var tableView: UITableView!
     
+    let myRefreshControl = UIRefreshControl()
+    
     var posts = [PFObject]()
+    var refreshControl: UIRefreshControl!
+    var numberOfPost: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        numberOfPost = 20
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let query = PFQuery(className:"Posts")
-        query.includeKey("author")
-        query.limit = 20
-        
-        query.findObjectsInBackground { (posts, error) in
-            if posts != nil {
-                self.posts = posts!
-                self.tableView.reloadData()
-            } else {
-                print("Cound't load data: \(error?.localizedDescription)")
-            }
-        }
+        self.loadPosts()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,6 +60,46 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.photoView.af_setImage(withURL: url)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            loadMorePosts()
+        }
+    }
+    
+    func loadPosts() {
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = numberOfPost
+        query.order(byDescending: "updatedAt")
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            } else {
+                print("Cound't load data: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    func loadMorePosts() {
+        numberOfPost += 20
+        self.loadPosts()
+    }
+    
+    @objc func onRefresh() {
+       run(after: 2) {
+          self.numberOfPost = 20
+          self.loadPosts()
+          self.refreshControl.endRefreshing()
+       }
+    }
+    
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
 
     /*
